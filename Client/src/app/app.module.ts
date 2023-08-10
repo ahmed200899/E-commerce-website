3031360ec580c6a96829f475a66c9d2973c497aa
Ahmed Mohamed <ahmed20089912@outlook.com>
2023-08-10 13:43:57 +0300

    API is finished

---
+++ Client/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+/**
+ * The AppModule class is a module in an Angular application that imports necessary modules, declares components, and provides services. It also specifies the root component to bootstrap the application.
+ */
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -26,37 +26,41 @@ namespace Infrastructure.Data
                                 .Include(p=>p.ProductBrand)
                                 .FirstOrDefaultAsync(p=>p.Id == id);
         }
-        public async Task<IReadOnlyList<Product>> GetProductsAsnc(string order_by, string AscOrDesc,int? Typeid,int? brandid,int pagenumber,string search)
+        public async Task<IReadOnlyList<Product>> GetProductsAsync(string orderBy, string ascOrDesc, int? typeId, int? brandId, int pageNumber, string search)
         {
-            var products= _context.Products
-                            .Include(p=>p.ProductType)
-                            .Include(p=>p.ProductBrand)
-                            .AsQueryable();
-                            
-            if (Typeid.HasValue||brandid.HasValue ||!string.IsNullOrEmpty(search))
-            {
-                products = products
-                            .Where(p =>(!Typeid.HasValue ||p.ProductTypeId == Typeid)
-                                &&(!brandid.HasValue || p.ProductBrandId == brandid)
-                                &&(string.IsNullOrEmpty(search))|| p.Name.ToLower().Contains(search))
-                            .AsQueryable();
-            }
-            return (string.IsNullOrEmpty(order_by)&&string.IsNullOrEmpty(AscOrDesc)&&order_by!="price")
-             ? await products
-                    .OrderBy(p =>p.Name)
-                    .Skip(pagenumber*PageSize).Take(PageSize)
-                    .ToListAsync() 
-            :(AscOrDesc.Equals("asc",StringComparison.OrdinalIgnoreCase)) 
-                            ? await products
-                                        .OrderBy(p =>p.Price)
-                                        .Skip(pagenumber*PageSize).Take(PageSize)
-                                        .ToListAsync()
-                            : await products
-                                        .OrderByDescending(p =>p.Price)
-                                        .Skip(pagenumber*PageSize).Take(PageSize)
-                                        .ToListAsync();                          
-        }
+            var products = _context.Products
+                .Include(p => p.ProductType)
+                .Include(p => p.ProductBrand)
+                .AsQueryable();
 
+            if (typeId.HasValue || brandId.HasValue || !string.IsNullOrEmpty(search))
+            {
+                products = products.Where(p =>
+                    (!typeId.HasValue || p.ProductTypeId == typeId) &&
+                    (!brandId.HasValue || p.ProductBrandId == brandId) &&
+                    (string.IsNullOrEmpty(search) || p.Name.ToLower().Contains(search))
+                );
+            }
+
+            return string.IsNullOrEmpty(orderBy) && string.IsNullOrEmpty(ascOrDesc) && orderBy != "price"
+
+                ? await products.OrderBy(p => p.Name)
+                    .Skip(pageNumber * PageSize)
+                    .Take(PageSize)
+                    .ToListAsync()
+
+                : ascOrDesc.Equals("asc", StringComparison.OrdinalIgnoreCase)
+                
+                    ? await products.OrderBy(p => p.Price)
+                        .Skip(pageNumber * PageSize)
+                        .Take(PageSize)
+                        .ToListAsync()
+
+                    : await products.OrderByDescending(p => p.Price)
+                        .Skip(pageNumber * PageSize)
+                        .Take(PageSize)
+                        .ToListAsync();
+        }
         public async Task<IReadOnlyList<ProductBrand>> GetProductsBrandsAsnc()
         {
             return await _context.ProductBrands.ToListAsync() ;
